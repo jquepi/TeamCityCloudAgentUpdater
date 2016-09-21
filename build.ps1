e3e0837dadc81ef75e91f3f07b6e1b1bb49afc6a
Matt Richardson <matt@redgumtech.com.au>
2016-09-21 13:35:53 +1000

    Add more logging

--- build.ps1
+++ build.ps1
@@ -1,19 +1,33 @@
 & npm install
 
 # the phantomjs-prebuilt module doesn't include the windows binary. Download and add it in.
-Invoke-WebRequest -Uri "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip" -OutFile "phantomjs-2.1.1-windows.zip"
 
 $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
+$url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip"
+$downloadDest = "$PSScriptRoot/phantomjs-2.1.1-windows.zip"
+
+write-host "Downloading '$url' to '$downloadDest'"
+
+Invoke-WebRequest -Uri $url -OutFile $downloadDest
+
+write-host "Extracting '$downloadDest'"
+
 $shell = new-object -com shell.application
-$zip = $shell.NameSpace("$PSScriptRoot/phantomjs-2.1.1-windows.zip")
+$zip = $shell.NameSpace("$$downloadDest")
 foreach($item in $zip.items())
 {
     $shell.Namespace("$PSScriptRoot").copyhere($item)
 }
 
-Copy-Item "./phantomjs-2.1.1-windows/bin/phantomjs.exe" "./node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs.exe"
+write-host "Copying to node-modules"
 
-Remove-Item "./phantomjs-2.1.1-windows" -force -recurse
+Copy-Item "$PSScriptRoot/phantomjs-2.1.1-windows/bin/phantomjs.exe" "./node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs.exe"
 
-Remove-Item "./phantomjs-2.1.1-windows.zip" -force
+write-host "Removing directory"
+
+Remove-Item "$PSScriptRoot/phantomjs-2.1.1-windows" -force -recurse
+
+write-host "Removing zip file"
+
+Remove-Item $downloadDest -force
