3ab41bf0edc0a1a01747c4bd848286133e36eb6d
Matt Richardson <matt@redgumtech.com.au>
2016-09-21 13:18:52 +1000

    Expand-Archive not available on all versions of windows

--- build.ps1
+++ build.ps1
@@ -1,10 +1,16 @@
 & npm install
 
 # the phantomjs-prebuilt module doesn't include the windows binary. Download and add it in.
-
 Invoke-WebRequest -Uri "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip" -OutFile "phantomjs-2.1.1-windows.zip"
 
-Expand-Archive -Path "./phantomjs-2.1.1-windows.zip" -DestinationPath "." -force
+$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace("$PSScriptRoot/phantomjs-2.1.1-windows.zip")
+foreach($item in $zip.items())
+{
+    $shell.Namespace("$PSScriptRoot").copyhere($item)
+}
 
 Copy-Item "./phantomjs-2.1.1-windows/bin/phantomjs.exe" "./node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs.exe"
 
